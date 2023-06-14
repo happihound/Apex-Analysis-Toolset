a794ff0b0a035923d48eb1bf1bbd8946965a7758
happihound <bevanneib@gmail.com>
2023-06-14 18:01:44 -0400

    started on evo

--- EvoTracker.py
+++ EvoTracker.py
@@ -1,70 +1,80 @@
 import time
-import numpy as np
-import cv2 as cv2
+import cv2
 import matplotlib.pyplot as plt
 import glob
 import multiprocessing
 import easyocr
 from tqdm import tqdm
-from statsmodels.nonparametric.smoothers_lowess import lowess
-
+import ApexUtils
+import numpy as np
 
 plt.switch_backend('TKAgg')
 plt.bbox_inches = "tight"
 
 
-def evoTracker(pathToImages, queuedImage, end):
-    # Define variables
-    end.value = 0
-    matchCount = 0
-    loopNumber = 0
-    imageCount = 0
-    foundData = []
-    imageNumber = []
-    foundData.append(0)
-    imageNumber.append(0)
+class evoTracker:
 
-    # Get image count
-    for _ in glob.glob(pathToImages + '/*.png'):
-        imageCount = imageCount + 1
-    print('Number of evo images to match ' + str(imageCount))
+    __slots__ = ['end', 'queued_image', 'loop_number', 'match_count',
+                 'results', 'result_image_number', 'reader', 'path_to_images', 'apex_utils']
 
-    # Create the easyocr reader
-    reader = easyocr.Reader(['en'])
+    def __init__(self):
+        self.end = 0
+        self.queued_image = None
+        self.loop_number = 0
+        self.match_count = 0
+        self.results = [0]
+        self.result_image_number = [0]
+        self.reader = None
+        self.apex_utils = ApexUtils.ApexUtils()
+        self.path_to_images = 'input/playerEvo'
 
-    # Use tqdm to display a progress bar
-    pbar = tqdm(glob.glob(pathToImages + '/*.png'))
+    def track_evo(self, queued_image, end):
+        self.reader = easyocr.Reader(['en'])
+        self.end = end
+        self.queued_image = queued_image
+        files = glob.glob(self.path_to_images + '/*.png')
+        with tqdm(files) as pbar:
+            for file in pbar:
+                image = cv2.imread(file)
+                self.loop_number += 1
+                result_OCR = self.reader.readtext(image, allowlist='0123456789', paragraph=False)
+                if self.process_result(result_OCR, pbar):
+                    self.queued_image.put(image)
+        print(f'found {self.match_count} total matches')
+        self.apex_utils.save(data=self.results, frame=self.result_image_number,
+                             headers=["Frame", "Evo"], name='Evo')
+        end.value = True
 
-    # Loop through all evo images
-    for file in pbar:
-        image = cv2.imread(file)
-        loopNumber = loopNumber + 1
+    def process_result(self, result_OCR, pbar):
+        for bbox, text, prob in result_OCR:
+            evo_found = int(text)
+            if prob > 0.93 and self.is_valid_damage(evo_found, pbar):
+                self.match_count += 1
+                self.results.append(evo_found)
+                self.result_image_number.append(self.loop_number)
+                pbar.set_description(desc=str(self.match_count), refresh=True)
+                return True
+        return False
 
-        # Define permitted characters
-        result = reader.readtext(
-            image, allowlist='0123456789', paragraph=False)
+    def is_valid_evo(self, evo_found, pbar):
+        if self.results[-1] > evo_found or self.results[-1] + 400 < evo_found:
+            pbar.write(f'Bad match, found {evo_found} but expected closer to '
+                       f'{self.results[-1]} at image {self.loop_number}')
+            return False
+        return True
+    
+    def filterValues(values):
+        # Make a copy of the list to avoid changing the original
+        newValues = values.copy()
 
-        # Check if the image matches the expected format
-        if check_match(result, loopNumber, foundData, imageNumber, pbar, matchCount):
-            # Add the image to the queue
-            queuedImage.put(image)
-            matchCount = matchCount + 1
+        # Loop through the list, skipping the first and last values
+        for i in range(1, len(values) - 1):
+            # If the next value is the same as the previous value, set the current value to that value
+            if values[i - 1] == values[i + 1]:
+                newValues[i] = values[i - 1]
 
-    # Add the final data to the arrays
-    imageNumber.append((imageNumber[-1] + 1))
-    foundData.append(0)
-    foundData.append(0)
-    foundData = filterValues(foundData)
-    imageNumber.append(imageCount)
-    print('found ' + str(matchCount) + ' total matches')
-
-    # Save the data
-    save(foundData, imageNumber)
-
-    # Graph the data
-    graph(foundData, imageNumber)
-    # End the program
-    end.value = 1
+        # Return the new list
+        return newValues
 
 
 def check_match(result, loopNumber, foundData, imageNumber, pbar, matchCount):
@@ -91,76 +101,15 @@ def check_match(result, loopNumber, foundData, imageNumber, pbar, matchCount):
     # Return false to say we didn't find a match
     return False
 
-
-def save(matchedImages, matchingImages):
-    np.save('outputdata/evoData.npy',
-            np.vstack((matchedImages, matchingImages)))
-
-
-def filterValues(values):
-    # Make a copy of the list to avoid changing the original
-    newValues = values.copy()
-
-    # Loop through the list, skipping the first and last values
-    for i in range(1, len(values) - 1):
-        # If the next value is the same as the previous value, set the current value to that value
-        if values[i - 1] == values[i + 1]:
-            newValues[i] = values[i - 1]
-
-    # Return the new list
-    return newValues
-
-
-def graph(foundData, imageNumber):
-    # Creates a graph
-    total = imageNumber[-1]
-    x, y = (smoothing(foundData, imageNumber))
-    plt.plot(x, y)
-    plt.xlabel("Time")
-    plt.ylabel("Damage to level up")
-    plt.xlim([0, total])
-    plt.show()
-
-
-def smoothing(y, x):
-    # Smooth the data
-    # fraction of data to be used for estimating the smoothed values
-    lowess_frac = 0.0001
-    # number of iterations for the lowess algorithm
-    lowess_it = 0
-    # set x_smooth to be the same as x
-    x_smooth = x
-    # calculate smoothed values
-    y_smooth = lowess(y, x, is_sorted=False, frac=lowess_frac,
-                      it=lowess_it, return_sorted=False)
-    return x_smooth, y_smooth
-
-
-def display(queuedImage, end):
-    time.sleep(1)
-    cv2.namedWindow('evoTracker')
-    cv2.imshow("evoTracker", cv2.imread('inputData/default.png'))
-    while not end.value:
-        if not queuedImage.empty():
-            cv2.imshow("evoTracker", queuedImage.get())
-            continue
-        cv2.waitKey(1)
-
-
-def main():
-    print('Starting evo tracker')
-    queuedImage = multiprocessing.Queue()
-    end = multiprocessing.Value("i", False)
-    pathToImages = 'inputData/playerEvo/'
-    process1 = multiprocessing.Process(
-        target=evoTracker, args=(pathToImages, queuedImage, end))
-    process2 = multiprocessing.Process(target=display, args=(queuedImage, end))
-    process1.start()
-    process2.start()
-    process1.join()
-    process2.join()
-    print('Finished evo tracker')
+    def main(self):
+        print('Starting damage tracker')
+        end = multiprocessing.Value("i", False)
+        queued_image = multiprocessing.Queue()
+        self.apex_utils.display(queued_image, end, 'Damage Tracker')
+        self.track_damage(queued_image, end)
+        print('Finished damage tracker')
 
 
 if __name__ == '__main__':
-    main()
+    damage_tracker = DamageTracker()
+    damage_tracker.main()
