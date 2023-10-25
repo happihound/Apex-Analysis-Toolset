3295c3c0a97358885e655b1b5c06f1c0c22985d2
happihound <bevanneib@gmail.com>
2023-10-25 13:47:32 -0400

    got stuff working

--- src/static/js/console.js
+++ src/static/js/console.js
@@ -1,3 +1,36 @@
+function createCollapsibleConsole() {
+    // Create button element
+    var button = document.createElement('button');
+    button.classList.add('collapsible', 'collapsible-console');
+
+    var icon = document.createElement('i');
+    icon.classList.add('fas', 'fa-chevron-down', 'chevron-icon');
+    button.appendChild(icon);
+
+    // Create span element for text
+    var span = document.createElement('span');
+    span.textContent = 'Console Output';
+    button.appendChild(span);
+
+    // Create Font Awesome icon element
+
+    // Create content div
+    var contentDiv = document.createElement('div');
+    contentDiv.classList.add('content');
+
+    // Create pre element for console output
+    var consoleOutput = document.createElement('pre');
+    consoleOutput.id = 'console-output';
+
+    // Append console output to content div
+    contentDiv.appendChild(consoleOutput);
+
+    // Append button and content div to body (or another parent element)
+    var parentElement = document.getElementById('insert_console');
+    parentElement.appendChild(button);
+    parentElement.appendChild(contentDiv);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document is loaded');
 
@@ -13,9 +46,17 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Console Output received:', message.data);
         var consoleElement = document.getElementById('console-output');
         consoleElement.textContent = message.data.join('\n');
+        //Cut the text if it goes over 12 lines
+        var lines = consoleElement.textContent.split('\n');
+        if (lines.length > 12) {
+            consoleElement.textContent = lines.slice(lines.length - 12).join('\n');
+        }
     });
 
-    
+    // Call function to create and inject the collapsible console HTML
+    createCollapsibleConsole();
+
+    // Collapsible button event listener setup
     var coll = document.getElementsByClassName("collapsible-console");
     console.log('Number of collapsible-console elements:', coll.length);
     for (var i = 0; i < coll.length; i++) {
